6d97b54f221fcec48f1116eb782cd27d98cb3ff8
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-31 00:32:31 -0300

    :sparkles: adicionando tela de login a aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoComponent } from './components/tecnico/tecnico.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: NavComponent, children: [
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'tecnico',
+        component: TecnicoComponent
       }
     ]
   },
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email: FormControl = new FormControl('', Validators.email)
+  senha: FormControl = new FormControl('', Validators.min(3));
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  logar(){
+
+  }
+
+  validaCampos(): boolean {
+    return this.email.valid && this.senha.valid;
   }
 
 }
